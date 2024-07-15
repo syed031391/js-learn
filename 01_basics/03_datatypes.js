fdb342db99ae080531b0787440258fed0b4bd61d
syed031391 <113092727+syed031391@users.noreply.github.com>
2024-07-15 09:21:57 +0000

    datatypes

---
+++ 01_basics/03_datatypes.js
@@ -0,0 +1,4 @@
+/* there two data types in js Primitive and non Primitive 
+1 Primitive type include boolean, symbol ,number ,string, bigInt, null, undefined
+2 Non primitive type  is abject and function , array
+*/
