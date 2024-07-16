640810e920106b7c93afcbc6165c6d347effa656
syed031391 <113092727+syed031391@users.noreply.github.com>
2024-07-16 19:35:06 +0000

    Array part 1

---
+++ 01_basics/04_math_and_num.js
@@ -0,0 +1,7 @@
+const num = 2.2;
+// console.log(Math.ceil(num)); math.ceil give the upper value.
+// console.log(Math.floor(num)); math.floor give the lower value.
+// console.log(Math.round(num)); math.round use to round of the value
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(min-max+1)+min));
---
+++ 02_basics/01_array.js
@@ -0,0 +1,9 @@
+// A collection value or element in a singal variable is called array.
+const array  = [1,2,3,4,5,6,7,8];
+// push method is use to add one or more element to the end of an array.
+array.push(9)
+//Pop: The pop method removes the last element from an array.
+array.pop()
+// shift method is use to remove the firt element of the array
+array.shift()
+console.log(array);
