da8241e458be142d3a24e3374dcd9d4379b718bd
syed031391 <113092727+syed031391@users.noreply.github.com>
2024-07-16 09:58:13 +0000

    String

---
+++ 01_basics/02-string.js
@@ -0,0 +1,10 @@
+const string = '    waqar'
+const repoCount = 50
+// console.log(`hi this is ${string} and i have done ${repoCount}`);
+// console.log(string.split(''));
+// console.log(string.charAt(2));
+// console.log(string.indexOf('w'));
+// console.log(string.includes('s'));
+// console.log(string.replace('r','s'));
+// console.log(string.toUpperCase());
+// console.log(string.trim());
