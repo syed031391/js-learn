51b6c04b860de6f74d2bb7e95391d4c17c365b50
syed031391 <113092727+syed031391@users.noreply.github.com>
2024-07-17 09:54:21 +0000

    Object part 1

---
+++ 02_basics/03_object.js
@@ -0,0 +1,13 @@
+const jsUser = {
+    name: 'waqar',
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+    jsUser.greeting = function()
+{
+    console.log(`hello js user ${this.name}`)
+}
+
+console.log(jsUser.greeting())
