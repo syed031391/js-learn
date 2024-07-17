5eff5f6914be3581c9a41037852a8c945eead8e5
syed031391 <113092727+syed031391@users.noreply.github.com>
2024-07-17 08:36:16 +0000

    array part 2

---
+++ 02_basics/02_array.js
@@ -0,0 +1,10 @@
+let fruits = ["Apple", "Banana"];
+let vegetables = ["Carrot", "Broccoli"];
+
+// sperad opperator is use to shollow copy of the array .for combine the two array.
+
+let combine = [...fruits,...vegetables];
+// cancat method is use to combine two array with out modifile the orignal array and creat new array.
+let combine2 = fruits.concat(vegetables)
+console.log(combine2);
+
