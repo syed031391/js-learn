d1a0e48b99bc7df6b94cda942a77c468f3af0242
syed031391 <113092727+syed031391@users.noreply.github.com>
2024-07-18 13:48:01 +0000

    for loop part 2

--- 05_iteration/01_for_loop.js
+++ 05_iteration/01_for_loop.js
@@ -1,4 +1,19 @@
-for (let index = 1; index < 10; index++) {
-    const element = index;
-    console.log(element)
+
+
+const table = (table)=>
+{
+    for (let j = 1; j <=10; j++) {
+   
+        console.log(`${table} * ${j} = ${j*table} `)
+    }
+}
+// table(3,2)
+// multiplication table from 1 to 10
+
+for (let i = 1; i <=10; i++) {
+     
+    console.log(`**********Table of ${i}**********`)
+    for (let j = 1; j <=10; j++) {
+        console.log(`${i} * ${j} = ${j*i} `)    
+    }
 }
